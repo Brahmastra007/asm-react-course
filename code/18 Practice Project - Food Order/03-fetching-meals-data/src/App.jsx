615f0578062065b7dacac1ff695af25c46b4f2f4
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-04-10 20:30:05 +0530

    C18L03 - Fetching meals data

--- code/18 Practice Project - Food Order/03-fetching-meals-data/src/App.jsx
+++ code/18 Practice Project - Food Order/03-fetching-meals-data/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header.jsx';
 import Meals from './components/Meals.jsx';
 
 function App() {
+  // We add the 'Meals' component here
   return (
     <>
       <Header />
--- code/18 Practice Project - Food Order/03-fetching-meals-data/src/components/Meals.jsx
+++ code/18 Practice Project - Food Order/03-fetching-meals-data/src/components/Meals.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react';
 
+// We create the 'Meals' component for displaying all the meals available
 export default function Meals() {
+  // State for managing all the meals loaded from the backend
   const [loadedMeals, setLoadedMeals] = useState([]);
 
+  // Fetching the meals data from backend via HTTP request
+  // Using 'useEffect' hook so that we don't cause infinite loop
+  // No dependencies present which means that the hook will run exactly once after component renders
   useEffect(() => {
     async function fetchMeals() {
+      // Fetching meals data
       const response = await fetch('http://localhost:3000/meals');
 
       if (!response.ok) {
@@ -12,6 +18,7 @@ export default function Meals() {
       }
 
       const meals = await response.json();
+      // Setting received meals data in the 'loadedMeals' state
       setLoadedMeals(meals);
     }
 
