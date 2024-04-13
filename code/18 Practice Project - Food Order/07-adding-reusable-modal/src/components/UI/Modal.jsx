8669ddd56bc48ac357f3b27a6b865d182927225d
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-04-13 18:59:37 +0530

    C18L07 - Adding reusable model

--- code/18 Practice Project - Food Order/07-adding-reusable-modal/src/components/UI/Modal.jsx
+++ code/18 Practice Project - Food Order/07-adding-reusable-modal/src/components/UI/Modal.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
+// Defining a Modal component to be used anywhere
 export default function Modal({ children, open, className = '' }) {
+  // Creating a ref to the dialog element
   const dialog = useRef();
 
+  // Using useEffect hook so that the modal is opened programatically whenever 'open' prop is changed
   useEffect(() => {
     if (open) {
       dialog.current.showModal();
     }
   }, [open]);
 
+  // Using Portal to use this modal in any component but while inserting it in the DOM
+  // in the element with ID 'modal'
   return createPortal(
     <dialog ref={dialog} className={`modal ${className}`}>
       {children}
