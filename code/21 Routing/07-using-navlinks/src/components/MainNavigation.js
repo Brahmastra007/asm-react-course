d129a179c8b53a9de8c5cfbc3938333df3c0f372
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-04-16 09:44:51 +0530

    C21L07 - Using navlinks

--- code/21 Routing/07-using-navlinks/src/components/MainNavigation.js
+++ code/21 Routing/07-using-navlinks/src/components/MainNavigation.js
@@ -8,14 +8,20 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
+            {/* Using 'Navlink' to get functionality based on if links are active or not */}
             <NavLink
               to="/"
+              // Adding a function in 'className' which will apply different classes based on if
+              // the link is active or not
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
+              // We can do the same for styling
               // style={({ isActive }) => ({
               //   textAlign: isActive ? 'center' : 'left',
               // })}
+
+              // This keyword determines if the end of current url should also match this url
               end
             >
               Home
