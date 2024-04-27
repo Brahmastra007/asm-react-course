b7e5b4534a7bc470bf7f45a0decec5e544ff728b
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-04-27 16:03:55 +0530

    C21L18 - User feedback useNavigation

--- code/21 Routing/18-user-feedback-usenavigation/src/pages/Root.js
+++ code/21 Routing/18-user-feedback-usenavigation/src/pages/Root.js
@@ -3,12 +3,16 @@ import { Outlet, useNavigation } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
+  // We can use the 'useNavigation' hook to get the current navigation state, e.g. if we are
+  // loading or not loading any route.
   // const navigation = useNavigation();
 
   return (
     <>
       <MainNavigation />
       <main>
+        {/* If we are currently in the loading navigation state and have not actually navigated
+        to the selected route, we can show a loading message. */}
         {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
         <Outlet />
       </main>
