b8bb8181ca9aa0acb4eb2a9f1b4f998b4cf7c987
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-04-28 13:22:08 +0530

    C21L19 - Returning responses

--- code/21 Routing/19-returning-responses/src/pages/Events.js
+++ code/21 Routing/19-returning-responses/src/pages/Events.js
@@ -4,6 +4,7 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const data = useLoaderData();
+  // Getting our events data from the data returned by the HTTP request
   const events = data.events;
 
   return <EventsList events={events} />;
@@ -17,6 +18,10 @@ export async function loader() {
   if (!response.ok) {
     // ...
   } else {
+    // The 'fetch' function returns a 'Promise' of a 'Response' object.
+    // React router will first wait for the promise to get resolved after which we will get a
+    // 'Response' object. React is also configured to return the data from that response.
+    // Hence we can directly return the response and React will automatically get the data from it.
     return response;
   }
 }
