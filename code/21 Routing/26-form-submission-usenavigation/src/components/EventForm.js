775d053ec60653a2879d1fe84e1ce0794da447ee
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-04-28 20:19:53 +0530

    C21L26 - Form submission useNavigation

--- code/21 Routing/26-form-submission-usenavigation/src/components/EventForm.js
+++ code/21 Routing/26-form-submission-usenavigation/src/components/EventForm.js
@@ -4,8 +4,10 @@ import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  // We can get the current navigation state using this hook
   const navigation = useNavigation();
 
+  // Determining if we are currently submitting the data, i.e. executing the action
   const isSubmitting = navigation.state === 'submitting';
 
   function cancelHandler() {
@@ -55,10 +57,12 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
+        {/* Buttons are disabled while submitting the data */}
         <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
         <button disabled={isSubmitting}>
+          {/* Displaying different text while submitting the data */}
           {isSubmitting ? 'Submitting...' : 'Save'}
         </button>
       </div>
