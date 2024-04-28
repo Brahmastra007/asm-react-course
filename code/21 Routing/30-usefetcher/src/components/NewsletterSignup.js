cbd37073dcedd05dd98cb6c58d0ca27060e7bfa6
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-04-28 22:49:14 +0530

    C21L30 - useFetcher

--- code/21 Routing/30-usefetcher/src/components/NewsletterSignup.js
+++ code/21 Routing/30-usefetcher/src/components/NewsletterSignup.js
@@ -4,9 +4,13 @@ import { useFetcher } from 'react-router-dom';
 import classes from './NewsletterSignup.module.css';
 
 function NewsletterSignup() {
+  // We can use the 'useFetcher' hook to use the loader or action functions defined for some
+  // other route but don't want to redirect to that route which will happen if we just use 'Form'
   const fetcher = useFetcher();
+  // We get the data returned and current state of the loader/action
   const { data, state } = fetcher;
 
+  // Using 'useEffect' to show an alert if we have received some message from the loader/action
   useEffect(() => {
     if (state === 'idle' && data && data.message) {
       window.alert(data.message);
@@ -14,6 +18,7 @@ function NewsletterSignup() {
   }, [data, state]);
 
   return (
+    // Using 'fetcher.Form' will not redirect to the route specified in the 'action' property
     <fetcher.Form
       method="post"
       action="/newsletter"
