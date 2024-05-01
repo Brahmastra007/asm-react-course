6dccf9fcda467e39752a063d0db461dc541ec1d1
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-01 18:35:48 +0530

    C23L02 - Finished

--- code/23 Deployment/02-finished/src/App.js
+++ code/23 Deployment/02-finished/src/App.js
@@ -1,11 +1,15 @@
 import { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// To implement lazy loading, we should remove the direct imports for the components which have
+// not yet been navigated to by the user.
 // import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
 // import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
 
+// Lazily loading specific components by wrapping the imports in a lazy function. They will be loaded
+// (fetched from the server) only when the user navigates to them.
 const BlogPage = lazy(() => import('./pages/Blog'));
 const PostPage = lazy(() => import('./pages/Post'));
 
@@ -23,21 +27,27 @@ const router = createBrowserRouter([
         children: [
           {
             index: true,
+            // Wrapping the page in a 'Suspense' block with a fallback to suspense loading this
+            // component until the user navigates to it.
             element: (
               <Suspense fallback={<p>Loading...</p>}>
                 <BlogPage />
               </Suspense>
             ),
+            // The loader is also loaded and called only when the user navigates to this page. Here
+            // we must import the required module and then call the loader function.
             loader: () =>
               import('./pages/Blog').then((module) => module.loader()),
           },
           {
             path: ':id',
+            // Same thing as above
             element: (
               <Suspense fallback={<p>Loading...</p>}>
                 <PostPage />
               </Suspense>
             ),
+            // Here we also pass the arguments to the loader function
             loader: (meta) =>
               import('./pages/Post').then((module) => module.loader(meta)),
           },
