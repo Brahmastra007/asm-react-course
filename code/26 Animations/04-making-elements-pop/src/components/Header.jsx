75f751b066cc4c8677c101cdcf71d0d7031fa613
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-02 21:54:34 +0530

    C26L04 - Making elements pop

--- code/26 Animations/04-making-elements-pop/src/components/Header.jsx
+++ code/26 Animations/04-making-elements-pop/src/components/Header.jsx
@@ -23,7 +23,9 @@ export default function Header() {
       <header id="main-header">
         <h1>Your Challenges</h1>
         <motion.button
+          // Adding effect to scale up the button when we hover over it
           whileHover={{ scale: 1.1 }}
+          // Setting transition 'type' to 'spring' to give the animation a bouncy effect
           transition={{ type: 'spring', stiffness: 500 }}
           onClick={handleStartAddNewChallenge}
           className="button"
