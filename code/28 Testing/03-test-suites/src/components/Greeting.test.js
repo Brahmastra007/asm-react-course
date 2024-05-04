3b6f2f6aceb3461011fe6aec09269af74abc5531
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-04 20:44:13 +0530

    C28L03 - Test suites

--- code/28 Testing/03-test-suites/src/components/Greeting.test.js
+++ code/28 Testing/03-test-suites/src/components/Greeting.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
+// Adding a test suite to add all the tests related to some common functionality
 describe('Greeting component', () => {
   test('renders Hello World as a text', () => {
     // Arrange
