75add8cff47365f0806cca45334c32f29189e1b9
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-04 20:39:00 +0530

    C28L02 - Writing our first test

--- code/28 Testing/02-writing-our-first-test/src/components/Greeting.test.js
+++ code/28 Testing/02-writing-our-first-test/src/components/Greeting.test.js
@@ -3,12 +3,15 @@ import Greeting from './Greeting';
 
 test('renders Hello World as a text', () => {
   // Arrange
+  // Rendering the 'Greeting' component
   render(<Greeting />);
 
   // Act
   // ... nothing
 
   // Assert
+  // Getting the element by text
   const helloWorldElement = screen.getByText('Hello World!');
+  // Asserting that the element exists in the document
   expect(helloWorldElement).toBeInTheDocument();
 });
