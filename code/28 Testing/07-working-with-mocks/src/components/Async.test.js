d6e1c705257c4b6b185f0481ee3b800bd80c5859
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-05 13:50:43 +0530

    C28L07 - Working with mocks

--- code/28 Testing/07-working-with-mocks/src/components/Async.test.js
+++ code/28 Testing/07-working-with-mocks/src/components/Async.test.js
@@ -3,7 +3,11 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('renders posts if request succeeds', async () => {
+    // We can also mock functions such as 'fetch' to allow it to return a mock data instead of
+    // actually sending the HTTP response to get the data.
     window.fetch = jest.fn();
+    // This will mock the 'fetch' function to resolve to the given value once. We provide a 'json'
+    // key which contains an async function as the value which will get resolved and provide the data.
     window.fetch.mockResolvedValueOnce({
       json: async () => [{ id: 'p1', title: 'First post' }],
     });
