f14b65678331da658c685806becc93b9998b1b08
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-06 10:17:11 +0530

    C24L04 - Enabling disabling queries

--- code/24 React Query/04-enabling-disabling-queries/src/components/Events/FindEventSection.jsx
+++ code/24 React Query/04-enabling-disabling-queries/src/components/Events/FindEventSection.jsx
@@ -8,21 +8,34 @@ import EventItem from './EventItem.jsx';
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  // Setting an initial search term of 'undefined'
   const [searchTerm, setSearchTerm] = useState();
 
+  // Using 'isLoading' which will be false when the query is disabled, unlike 'isPending' which
+  // will be 'true' even when query is disabled
   const { data, isLoading, isError, error } = useQuery({
+    // Using a dynamic query key to store data corresponding to a particular search term
     queryKey: ['events', { search: searchTerm }],
+    // This query function is passed an object (containing 'signal', 'queryKey') for additional
+    // functionality, hence we should attach our search term with that object.
     queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+    // We can disable the query to not send a request.
+    // Initially this query would be disabled, but would be enabled afterwards for every submission
+    // since the search term would be a string (empty or non-empty).
     enabled: searchTerm !== undefined
   });
 
   function handleSubmit(event) {
     event.preventDefault();
+    // Setting the search term state whenever the form is submitted so that the component is
+    // re-rendered and the data fetch request is sent again.
     setSearchTerm(searchElement.current.value);
   }
 
   let content = <p>Please enter a search term and to find events.</p>;
 
+  // Setting 'isLoading' here instead of 'isPending' so that we don't show a loading indicator
+  // initially when the query is disabled.
   if (isLoading) {
     content = <LoadingIndicator />;
   }
--- code/24 React Query/04-enabling-disabling-queries/src/util/http.js
+++ code/24 React Query/04-enabling-disabling-queries/src/util/http.js
@@ -1,11 +1,15 @@
+// Accepting a signal parameter (along with the search term) that can abort the request, e.g. when
+// we move out of the page before the request is completed.
 export async function fetchEvents({ signal, searchTerm }) {
   console.log(searchTerm);
   let url = 'http://localhost:3000/events';
 
+  // Modifying the url if a search term was sent
   if (searchTerm) {
     url += '?search=' + searchTerm;
   }
 
+  // Sending the signal here with the request
   const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
