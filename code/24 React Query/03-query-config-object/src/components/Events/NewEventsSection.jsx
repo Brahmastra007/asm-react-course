95c50fcd9b0fbfcc43196bd6a6b2f727e40f5a4a
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-06 09:46:57 +0530

    C24L03 - Query config object

--- code/24 React Query/03-query-config-object/src/components/Events/NewEventsSection.jsx
+++ code/24 React Query/03-query-config-object/src/components/Events/NewEventsSection.jsx
@@ -9,7 +9,14 @@ export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events'],
     queryFn: fetchEvents,
+    // This is the amount of time under which a new request will not be sent to fetch the data.
+    // This means if we fetched the data at 0s, and then we went to another page and then came back
+    // to this page before 5s, the cached data will be shown and a new request would not be sent to
+    // fetch the updated data. If we came back after 5s, we will be shown the cached data but a new
+    // request would also be sent to fetch the updated data.
     staleTime: 5000,
+    // This is the amount of time for which the cached data is stored after which it will be cleared.
+    // After this interval, we could show the data only after we fetched it again.
     // gcTime: 1000
   });
 
