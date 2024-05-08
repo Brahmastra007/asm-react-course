227092d3c1112214512c47b3cc14a70be954e944
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-08 20:15:29 +0530

    C08L03 - Introducing refs connecting elements

--- code/08 Refs Portals/03-introducing-refs-connecting-elements/src/components/Player.jsx
+++ code/08 Refs Portals/03-introducing-refs-connecting-elements/src/components/Player.jsx
@@ -1,11 +1,13 @@
 import { useState, useRef } from 'react';
 
 export default function Player() {
+  // Using 'useRef' hook to get a reference/connection to an element
   const playerName = useRef();
 
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
   
   function handleClick() {
+    // Set player name from ref on submission
     setEnteredPlayerName(playerName.current.value);
   }
 
@@ -14,6 +16,7 @@ export default function Player() {
       <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
         <input
+          // Setting connection with this element through this ref
           ref={playerName}
           type="text"
         />
