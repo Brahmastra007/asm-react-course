340e56a0b4742ca7753fec8c5b73e62e1457f67e
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-10 14:09:00 +0530

    C08L10 - Portals finished

--- code/08 Refs Portals/10-portals-finished/src/components/ResultModal.jsx
+++ code/08 Refs Portals/10-portals-finished/src/components/ResultModal.jsx
@@ -7,8 +7,11 @@ const ResultModal = forwardRef(function ResultModal(
 ) {
   const dialog = useRef();
 
+  // If remaining time is <= 0, then the user has lost
   const userLost = remainingTime <= 0;
+  // Formatting remaining time to show only 2 decimal places
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  // Calculating score
   const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
@@ -19,8 +22,10 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
 
+  // Using portal to show this dialog not in the normal layout flow, but in some other place
   return createPortal(
     <dialog ref={dialog} className="result-modal">
+      {/* Showing different info according to if the user won or lost */}
       {userLost && <h2>You lost</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
       <p>
@@ -30,10 +35,12 @@ const ResultModal = forwardRef(function ResultModal(
         You stopped the timer with{' '}
         <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
+      {/* On submitting the form, we reset the timer through the 'onReset' function received in props */}
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>,
+    // Displaying the dialog in this element which is on the outermost level in the DOM
     document.getElementById('modal')
   );
 });
--- code/08 Refs Portals/10-portals-finished/src/components/TimerChallenge.jsx
+++ code/08 Refs Portals/10-portals-finished/src/components/TimerChallenge.jsx
@@ -8,26 +8,37 @@ export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
+  // Store the time remaining before the timer expires in a state
   const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
+  // Determining if timer is active
   const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
+  // Here the timer has expired before the user could click to stop the timer.
   if (timeRemaining <= 0) {
+    // Clearing the interval and opening the result modal to show that the user has lost
     clearInterval(timer.current);
     dialog.current.open();
   }
 
+  // Function to reset the timer
   function handleReset() {
+    // To reset the timer, we reset the time remaining
     setTimeRemaining(targetTime * 1000);
   }
 
   function handleStart() {
+    // To keep track of remaining time, we have to use 'setInterval' instead of 'setTimeout' as it
+    // performs some action repeatedly after some defined interval (here that interval is 10ms).
     timer.current = setInterval(() => {
+      // Updating the remaining time
       setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
     }, 10);
   }
 
+  // Function to stop the timer and show the score
   function handleStop() {
+    // Opening the dialog to show the score and clearing the interval
     dialog.current.open();
     clearInterval(timer.current);
   }
@@ -37,7 +48,9 @@ export default function TimerChallenge({ title, targetTime }) {
       <ResultModal
         ref={dialog}
         targetTime={targetTime}
+        // Remaining time
         remainingTime={timeRemaining}
+        // Function to reset the timer when the user closes the modal
         onReset={handleReset}
       />
       <section className="challenge">
@@ -45,6 +58,7 @@ export default function TimerChallenge({ title, targetTime }) {
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
+        {/* Changing conditions to use new variable 'timerIsActive' */}
         <p>
           <button onClick={timerIsActive ? handleStop : handleStart}>
             {timerIsActive ? 'Stop' : 'Start'} Challenge
