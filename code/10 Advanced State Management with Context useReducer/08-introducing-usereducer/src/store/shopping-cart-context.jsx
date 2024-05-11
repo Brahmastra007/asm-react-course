717fdb81669942c1aa57de2fc1d6f31b7f8c9758
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-05-11 16:52:00 +0530

    C10L08 - Introducing useReducer

--- code/10 Advanced State Management with Context useReducer/08-introducing-usereducer/src/store/shopping-cart-context.jsx
+++ code/10 Advanced State Management with Context useReducer/08-introducing-usereducer/src/store/shopping-cart-context.jsx
@@ -8,13 +8,19 @@ export const CartContext = createContext({
   updateItemQuantity: () => {},
 });
 
+// Defining the reducer function to modify the cart state. The 'state' we get here is guaranteed to
+// be the latest state snapshot. The 'action' is any information we send with the dispatch action.
 function shoppingCartReducer(state, action) {
   return state;
 }
 
 export default function CartContextProvider({ children }) {
+  // Using the 'useReducer' hook to manage the state which returns a state object and a dispatch
+  // function for dispatching actions to modify the state.
   const [shoppingCartState, shoppingCartDispatch] = useReducer(
+    // Reducer function
     shoppingCartReducer,
+    // Initial state
     {
       items: [],
     }
@@ -81,6 +87,7 @@ export default function CartContextProvider({ children }) {
   }
 
   const ctxValue = {
+    // Sending state value managed by 'useReducer'
     items: shoppingCartState.items,
     addItemToCart: handleAddItemToCart,
     updateItemQuantity: handleUpdateCartItemQuantity,
