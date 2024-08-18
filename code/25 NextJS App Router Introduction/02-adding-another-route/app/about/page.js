c93732e24cdaa5eb50ef4cb2cbec36b233143184
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-08-18 18:47:47 +0530

    C25L02 - Adding another route

--- code/25 NextJS App Router Introduction/02-adding-another-route/app/about/page.js
+++ code/25 NextJS App Router Introduction/02-adding-another-route/app/about/page.js
@@ -1,3 +1,5 @@
+// New routes can be added by adding a folder with the route name and defining a 'page.js' file
+// in it which will be executed on the server to render the page.
 export default function AboutPage() {
   return (
     <main>
