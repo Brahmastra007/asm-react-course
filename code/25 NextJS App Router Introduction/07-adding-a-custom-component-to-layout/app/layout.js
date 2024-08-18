7f7818256829137ca972f828aaa296d03f684d57
Anirudha Singh Mertia <anirudha@Anirudhas-MacBook-Pro.local>
2024-08-18 20:10:18 +0530

    C25L07 - Adding a custom component to layout

--- code/25 NextJS App Router Introduction/07-adding-a-custom-component-to-layout/app/layout.js
+++ code/25 NextJS App Router Introduction/07-adding-a-custom-component-to-layout/app/layout.js
@@ -31,6 +31,7 @@ export default function RootLayout({ children }) {
           </svg>
         </div>
 
+        {/* Adding the header component */}
         <MainHeader />
         {children}
       </body>
--- code/25 NextJS App Router Introduction/07-adding-a-custom-component-to-layout/components/main-header.js
+++ code/25 NextJS App Router Introduction/07-adding-a-custom-component-to-layout/components/main-header.js
@@ -5,11 +5,13 @@ import logoImg from '@/assets/logo.png';
 export default function MainHeader() {
   return (
     <header>
+      {/* Adding a logo image */}
       <Link href="/">
         <img src={logoImg.src} alt="A plate with food on it" />
         NextLevel Food
       </Link>
 
+      {/* Adding navigation with links to different routes */}
       <nav>
         <ul>
           <li>
