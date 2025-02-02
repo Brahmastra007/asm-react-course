7f7d5ca6dad4b75b6c00c4b93f4d84e420fcc8da
Anirudha Singh Mertia <anirudha.mertia@coupa.com>
2025-02-02 19:09:37 +0530

    C25L24 - Introducing using server actions

--- code/25 NextJS App Router Introduction/24-introducing-using-server-actions/app/meals/share/page.js
+++ code/25 NextJS App Router Introduction/24-introducing-using-server-actions/app/meals/share/page.js
@@ -2,9 +2,13 @@ import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+  // A server action should also be an 'async' function
   async function shareMeal(formData) {
+    /* We can use the 'use server' directive inside this function to make sure that this
+    function runs only on the server. */
     'use server';
 
+    // Extract the meal data
     const meal = {
       title: formData.get('title'),
       summary: formData.get('summary'),
@@ -26,6 +30,10 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
+        {/* Assign the server action to this form. When this form is submitted, behind the
+        scenes, Next.js will create a request and send it to the Next.js backend server.
+        This will cause this function to get triggered with the form data so that we can
+        handle the form submission there. */}
         <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
@@ -54,6 +62,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
+          {/* Add the label and name to the image picker */}
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
