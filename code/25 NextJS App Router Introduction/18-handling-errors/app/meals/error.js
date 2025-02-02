53c31bee0990c98002849f84eef2e81fe07ef5dd
Anirudha Singh Mertia <anirudha.mertia@coupa.com>
2025-02-02 14:19:11 +0530

    C25L18 - Handling errors

--- code/25 NextJS App Router Introduction/18-handling-errors/app/meals/error.js
+++ code/25 NextJS App Router Introduction/18-handling-errors/app/meals/error.js
@@ -1,5 +1,8 @@
+/* We need to use 'use client' directive because Next.js wants to catch all errors in the
+server as well as on the client side. */
 'use client';
 
+// Error page with the reserved name 'error.js' which will be shown when an error occurs
 export default function Error() {
   return (
     <main className="error">
--- code/25 NextJS App Router Introduction/18-handling-errors/lib/meals.js
+++ code/25 NextJS App Router Introduction/18-handling-errors/lib/meals.js
@@ -5,6 +5,7 @@ const db = sql('meals.db');
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
+  // Simulate an error
   // throw new Error('Loading meals failed');
   return db.prepare('SELECT * FROM meals').all();
 }
