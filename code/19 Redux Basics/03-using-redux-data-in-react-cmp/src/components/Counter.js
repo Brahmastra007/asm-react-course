e80991b32c57691cbe5006ad7cf09e0aef154f20
Anirudha Singh Mertia <anirudha.mertia@coupa.com>
2025-04-20 09:15:34 +0530

    C19L03 - Using Redux data in React cmp

--- code/19 Redux Basics/03-using-redux-data-in-react-cmp/src/components/Counter.js
+++ code/19 Redux Basics/03-using-redux-data-in-react-cmp/src/components/Counter.js
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  /* Use the 'useSelector' hook to setup a subscription to the Redux store. This will reexecute this component whenever the Redux
+  state changes. Here, slice the state to obtain only the counter. */
   const counter = useSelector(state => state.counter);
 
   const toggleCounterHandler = () => {};
@@ -10,6 +12,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
+      {/* Add the counter */}
       <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
