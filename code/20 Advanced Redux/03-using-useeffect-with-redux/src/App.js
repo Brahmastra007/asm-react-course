dd9fff9483870ac4ee6813fe659f068738f3b475
Anirudha Singh Mertia <anirudha.mertia@coupa.com>
2025-04-20 18:11:46 +0530

    C20L03 - Using useEffect with Redux

--- code/20 Advanced Redux/03-using-useeffect-with-redux/src/App.js
+++ code/20 Advanced Redux/03-using-useeffect-with-redux/src/App.js
@@ -7,8 +7,11 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  // Get the Redux cart state
   const cart = useSelector((state) => state.cart);
 
+  /* Whenever the cart will be updated (when an item is added or deleted), this 'useEffect' function will be called which will
+  make an API call to the firebase backend and update the cart information. */
   useEffect(() => {
     fetch('https://react-http-6b4a6.firebaseio.com/cart.json', {
       method: 'PUT',
