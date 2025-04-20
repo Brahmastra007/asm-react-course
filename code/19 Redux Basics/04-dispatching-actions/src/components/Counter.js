84de011855e74e6984ef5e5d96b2fa21c82e445b
Anirudha Singh Mertia <anirudha.mertia@coupa.com>
2025-04-20 09:29:56 +0530

    C19L04 - Dispatching actions

--- code/19 Redux Basics/04-dispatching-actions/src/components/Counter.js
+++ code/19 Redux Basics/04-dispatching-actions/src/components/Counter.js
@@ -3,13 +3,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // Hook used to dispatch actions to the Redux store
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
 
+  // Dispatch the 'increment' action
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
 
+  // Dispatch the 'decrement' action
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
@@ -21,6 +24,7 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
+        {/* Attach the counter handlers */}
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
